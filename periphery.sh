70896fc3604ce7cebbd02175f8d7c0e6b50d5527
Kristof <mrmaderat1@gmail.com>
2025-08-25 21:52:32 +0300

    Create periphery.sh

---
+++ periphery.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/FileEditor97/proxmox-stuff/main/misc/build.func)
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: FileEditor97
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/moghtech/komodo
+
+APP="Periphery"
+var_tags="${var_tags:-container;komodo;docker}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-8}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-12}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+    header_info
+    check_container_storage
+    check_container_resources
+    if [[ ! -d /usr/local/bin/periphery ]]; then
+        msg_error "No ${APP} Installation Found!"
+        exit
+    fi
+    msg_info "Updating ${APP} LXC"
+    $STD apt-get update
+    $STD apt-get -y upgrade
+    msg_ok "Updated Successfully"
+    exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
